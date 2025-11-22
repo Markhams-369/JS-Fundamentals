c3c3c0611a9273ae213f14f185ae3cfc6b0ccdd8
Markhams <tboitumelo868@gmail.com>
2025-11-23 00:49:04 +0200

    final push

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,4 +1,18 @@
 // Get the first argument
+// const arg = process.argv[2];
+// const num = parseInt(arg);
+
+// // Check if the argument is a valid integer
+// if (isNaN(num)) {
+//   console.log("Missing number of occurrences");
+// } else {
+//   let output = "";
+//   for (let i = 0; i < num; i++) {
+//     output += "C is fun";
+//   }
+//   console.log(output.trim());
+// }
+// Get the first argument
 const arg = process.argv[2];
 const num = parseInt(arg);
 
@@ -6,9 +20,11 @@ const num = parseInt(arg);
 if (isNaN(num)) {
   console.log("Missing number of occurrences");
 } else {
+  let i = 0;
   let output = "";
-  for (let i = 0; i < num; i++) {
-    output += "C is fun";
+  while (i < num) {
+    output += "C is fun\n";
+    i++;
   }
   console.log(output.trim());
 }
